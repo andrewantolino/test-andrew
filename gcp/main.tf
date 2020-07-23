79cd64ff88143d28981e715626b41b067e11f26a
andrewantolinoanz <68632196+andrewantolinoanz@users.noreply.github.com>
2020-07-23 18:15:13 +1000

    Create main.tf

---
+++ gcp/main.tf
@@ -0,0 +1,37 @@
+resource "google_container_cluster" "primary-cluster" {
+  name                     = "test-andrew"
+  location                 = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  #network                  = var.cluster_network
+  #subnetwork               = var.cluster_subnetwork
+}
+
+resource "google_container_node_pool" "preemptible_nodes" {
+  name       = "primary-pool"
+  location   = google_container_cluster.primary-cluster.location
+  cluster    = google_container_cluster.primary-cluster.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    #service_account = var.service_account
+
+    labels = {
+      machine-type = "preemtible"
+    }
+
+    tags = ["spark-cluster"]
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
